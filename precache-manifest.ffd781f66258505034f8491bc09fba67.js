45162ca3919e268e5e9397112c7fcf2eb20abf28
radu200 <raduprodan200@gmail.com>
2020-08-06 14:38:17 +0100

    Updates

---
+++ precache-manifest.ffd781f66258505034f8491bc09fba67.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d4acb79080b5c8c65f2fd370002b7ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33f43a81029c0de4b6b7",
+    "url": "/static/css/main.51db9f54.chunk.css"
+  },
+  {
+    "revision": "13bbfc5a61afc9095bc2",
+    "url": "/static/js/2.bf53b7c3.chunk.js"
+  },
+  {
+    "revision": "33f43a81029c0de4b6b7",
+    "url": "/static/js/main.31494a49.chunk.js"
+  },
+  {
+    "revision": "2a655c6dc63db212a7ea",
+    "url": "/static/js/runtime-main.b95ec835.js"
+  },
+  {
+    "revision": "027e218f86f7ce1b64d0959acb5ecfbc",
+    "url": "/static/media/aerlux.027e218f.png"
+  },
+  {
+    "revision": "cf8fd92bee71ddef15385a247c105ae9",
+    "url": "/static/media/flickr.cf8fd92b.png"
+  },
+  {
+    "revision": "6f2b94ef06a8f83cebd3dd6c1473633e",
+    "url": "/static/media/recruitment-app.6f2b94ef.png"
+  },
+  {
+    "revision": "be6362eb9d76c46ed4ee105cc28cb922",
+    "url": "/static/media/shop.be6362eb.png"
+  },
+  {
+    "revision": "c15ccc5c1f29dc1483656bc171831f90",
+    "url": "/static/media/weather-app.c15ccc5c.png"
+  }
+]);
